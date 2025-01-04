4b3140e4595b63ebaeb4f0688ba6ff7fb77f5010
Vika Vasilets <tbranz5@gmail.com>
2025-01-04 21:24:40 +0100

    Fix Straight flush

--- dealer.py
+++ dealer.py
@@ -3,6 +3,10 @@ class Dealer:
                  '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦', 'A♦',
                  '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠', 'A♠',
                  '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣', 'A♣']
+    ROYAL_DIAMONDS = ['10♦', 'J♦', 'Q♦', 'K♦', 'A♦']
+    ROYAL_HEARTS = ['10♥', 'J♥', 'Q♥', 'K♥', 'A♥']
+    ROYAL_SPADES = ['10♠', 'J♠', 'Q♠', 'K♠', 'A♠']
+    ROYAL_CLUBS = ['10♣', 'J♣', 'Q♣', 'K♣', 'A♣']
     SUITS = ['♥', '♦', '♠', '♣']
     RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
     RANK_SCORE = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
@@ -10,6 +14,7 @@ class Dealer:
 
     def __init__(self):
         self.deck = Dealer.ALL_CARDS.copy()
+        self.combo = []
 
     def create_deck(self):
         return [f"{rank}{suit}" for suit in SUITS for rank in RANKS]
@@ -23,21 +28,46 @@ class Dealer:
         return [self.deck.pop() for _ in range(num_cards)]
 
     def is_flush(self, hand):
-        suits = [card[-1] for card in hand]
+        suite_list = [card[-1] for card in hand]
+        count_map = {suite: suite_list.count(suite) for suite in suite_list}
+        sorted_count_map = sorted(count_map.items(), key=lambda item: item[1], reverse=True)
 
-        # Check if all suits are the same
-        return len(set(suits)) == 1
+        if sorted_count_map[0][1] >= 5:
+            self.combo = []
+            for card in sorted(hand, key=lambda card: Dealer.RANK_SCORE[card[0:-1]], reverse=True):
+                if sorted_count_map[0][0] in card and card not in self.combo:
+                    self.combo.append(card)
+                    if len(self.combo) == 5:
+                        break
+            return True
+        else:
+            return False
 
     def is_straight(self, hand):
         ranks = [card[0:-1] for card in hand]
-        rank_values = sorted(Dealer.RANK_SCORE[rank] for rank in ranks)
+        rank_values = sorted(sorted(Dealer.RANK_SCORE[rank] for rank in ranks), reverse=True)
 
         # Check for regular straight
-        if all(rank_values[i] + 1 == rank_values[i + 1] for i in range(len(rank_values) - 1)):
-            return True
+        step = 1
+        temp = []
+        for i in range(1, len(rank_values)):
+            if rank_values[i] + 1 == rank_values[i - 1]:
+                temp.append(rank_values[i])
+                temp.append(rank_values[i - 1])
+                step += 1
+                if step >= 5:
+                    self.combo = list(set(temp))
+                    return True
+            elif rank_values[i] == rank_values[i - 1]:
+                pass
+            else:
+                temp = []
+                step = 1
+        return False
 
         # Check for Ace-low straight (Ace, 2, 3, 4, 5)
         if set(rank_values) == {2, 3, 4, 5, 14}:
+            self.combo = {14, 2, 3, 4, 5};
             return True
         return False
 
@@ -59,12 +89,12 @@ class Dealer:
         count_map = self.calc_pairs(score_list);
         sorted_count_map = sorted(count_map.items(), key=lambda item: item[1], reverse=True)
 
-        if (self.is_flush(hand) and sum(score_list) == 60):
-            combination = 'Royal flush'
+        if (set(Dealer.ROYAL_HEARTS).issubset(hand) or
+            set(Dealer.ROYAL_CLUBS).issubset(hand) or
+            set(Dealer.ROYAL_SPADES).issubset(hand) or
+            set(Dealer.ROYAL_DIAMONDS).issubset(hand)):
+            combination = 'Royal flush!!!'
             return 60 * 100, combination
-        elif (self.is_flush(hand) and self.is_straight(hand)):
-            combination = 'Straight flush'
-            return sum(score_list) * 90, combination
         elif (sorted_count_map[0][1] == 4):
             print(f'Four of a kind! {self.find_key_by_val(sorted_count_map[0][0])}')
             return sum(score_list) * 80
@@ -72,12 +102,16 @@ class Dealer:
             combination = f'Full house {self.find_key_by_val(sorted_count_map[0][0])} and {self.find_key_by_val(sorted_count_map[1][0])}'
             return sum(score_list) * 70, combination
         elif (self.is_flush(hand)):
-            combination = f'Flush {sorted_hand[0]}'
-            return sum(score_list) * 60, combination
+            if self.is_straight(self.combo):
+                combination = f'Straight flush {self.combo}'
+                return sum(self.combo) * 90, combination
+            else:
+                combination = f'Flush {self.combo}'
+                return sum(score_list) * 60, combination
         elif (self.is_straight(hand)):
-            combination = f'Straight {sorted_hand[0]}'
-            return sum(score_list) * 50, combination
-        elif (sorted_count_map[0][1]  == 3):
+            combination = f'Straight {self.combo}'
+            return sum(self.combo) * 50, combination
+        elif (sorted_count_map[0][1] == 3):
             combination = f'Set {self.find_key_by_val(sorted_count_map[0][0])}'
             return sum(score_list) * 40, combination
         elif (sorted_count_map[0][1] == 2 and sorted_count_map[1][1] == 2):
--- main.py
+++ main.py
@@ -5,35 +5,26 @@ def play_texas_holdem():
     dealer = Dealer()
     dealer.shuffle_deck()
 
-    player1 = Player('Bot1')
-    player2 = Player('Bot2')
-    player3 = Player('Bot3')
+    player_list = []
+    player_list.append(Player('Bot1'))
+    player_list.append(Player('Bot2'))
+    player_list.append(Player('Bot3'))
 
-    player1.set_hand(dealer.deal_cards(2))
-    player2.set_hand(dealer.deal_cards(2))
-    player3.set_hand(dealer.deal_cards(2))
+    for player in player_list:
+        player.set_hand(dealer.deal_cards(2))
 
-    # Deal five community cards
     table_cards = dealer.deal_cards(5)
+    print("Table cards:", table_cards)
 
-    # Show hands and community cards
-    print(f"{player1.name}'s hand:", player1.hand)
-    print(f"{player2.name}'s hand:", player2.hand)
-    print(f"{player3.name}'s hand:", player3.hand)
-    print("Community cards:", table_cards)
+    for player in player_list:
+        print(f"{player.name}'s full hand:", player.hand + table_cards)
+        player.score, player.combination = dealer.calc_score(player.hand + table_cards);
+        print(f"{player.name}'s has combination: {player.combination} score: {player.score}")
 
-    print(f"{player1.name}'s full hand:", player1.hand + table_cards)
-    print(f"{player2.name}'s full hand:", player2.hand + table_cards)
-    print(f"{player3.name}'s full hand:", player3.hand + table_cards)
-
-    player1.score, player1.combination = dealer.calc_score(player1.hand + table_cards);
-    player2.score, player2.combination = dealer.calc_score(player2.hand + table_cards);
-    player3.score, player3.combination = dealer.calc_score(player3.hand + table_cards);
-
-    player_list = [player1, player2, player3]
-    winner = max(player_list , key=lambda player: player.score)
-    print(f"{winner.name} wins! combination: {winner.combination} score: {winner.score}")
-
+    max_score = max(player_list , key=lambda player: player.score).score
+    win_list = [player for player in player_list if player.score == max_score]
+    for player in win_list:
+        print(f"{player.name} wins! combination: {player.combination} score: {player.score}")
 
 if __name__ == "__main__":
     play_texas_holdem()
