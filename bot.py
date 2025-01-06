0d8f40d78ee6c88d8a3c43b91307d8cb71953a47
Vika Vasilets <tbranz5@gmail.com>
2025-01-06 21:29:58 +0100

    Added actions

--- bot.py
+++ bot.py
@@ -8,8 +8,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.types import CallbackQuery
 
 from dealer import Dealer
-from player import Player
-
+from player import Player, Action
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TOKEN)
@@ -17,14 +16,20 @@ dp = Dispatcher()
 dealer = Dealer()
 player_list = []
 table_cards = []
+msg_map = {}
+main_msg = types.Message
 
-inline_kb = InlineKeyboardMarkup(
-    inline_keyboard=[
-    [   InlineKeyboardButton(text="Deal a table", callback_data="table"),
-        InlineKeyboardButton(text="Deal a hand", callback_data="handshake"),
-        InlineKeyboardButton(text="Calc cards", callback_data="calc")]
-    ]
+start_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Join", callback_data="join")]])
+
+main_kb = InlineKeyboardMarkup(
+    inline_keyboard=[[InlineKeyboardButton(text="Leave", callback_data="leave")]]
 )
+
+round_kb = InlineKeyboardMarkup(
+    inline_keyboard=[[InlineKeyboardButton(text="Check", callback_data="check"),
+                      InlineKeyboardButton(text="Fold", callback_data="fold")]]
+)
+
 
 @dp.message(Command('info'))
 async def info(message: types.Message):
@@ -32,7 +37,6 @@ async def info(message: types.Message):
     await message.answer(f"Your chat ID is {chat_id}")
 
 
-@dp.message(Command('table'))
 async def deal_table(message: types.Message):
     try:
         global table_cards
@@ -42,32 +46,33 @@ async def deal_table(message: types.Message):
                 pass
             elif len(table_cards) >= 3:
                 table_cards.append(dealer.deal_cards(1)[0])
-                await message.edit_text(f"Table: {table_cards}", reply_markup=inline_kb)
         else:
             table_cards = dealer.deal_cards(3)
-            await message.edit_text(f"Table: {table_cards}", reply_markup=inline_kb)
+        await message.edit_text(gen_main_view_text(), reply_markup=main_kb)
     except Exception as e:
         await message.edit_text(f"Something goes wrong: {e}")
 
 
-@dp.message(Command('hand'))
 async def deal_hand(message: types.Message):
     try:
         global player_list
-        if isinstance(message, CallbackQuery):
-            user = message.from_user.first_name
-            message = message.message
-        else:
-            user = message.from_user.first_name
+        user = message.from_user.first_name
 
         if any(player.name == user for player in player_list):
             await message.answer(f"{user} already in game")
         else:
             player = Player(user)
+            player.id = message.from_user.id
             card = dealer.deal_cards(2)
             player.set_hand(card)
             player_list.append(player)
-            await message.answer(f"{user}'s hand: ||{card}||", parse_mode='MarkdownV2')
+
+            if isinstance(message, CallbackQuery):
+                message = message.message
+            await message.edit_text(gen_main_view_text(), parse_mode='MarkdownV2', reply_markup=main_kb)
+
+            msg = await message.answer(f"{user}'s hand: ||{card}||", parse_mode='MarkdownV2', reply_markup=round_kb)
+            msg_map[msg.message_id] = player.id
     except Exception as e:
         await message.answer(f"Something goes wrong: {e}")
 
@@ -80,13 +85,12 @@ async def players(message: types.Message):
         else:
             output = ''
             for player in player_list:
-                output = output + (f"{player.name}\n")
+                output = output + (f"{player.name} {player.id} {player.action.value}\n")
             await message.answer(output)
     except Exception as e:
         await message.answer(f"Something goes wrong: {e}")
 
 
-@dp.message(Command('check'))
 async def calc_cards(message: types.Message):
     try:
         global player_list
@@ -94,25 +98,28 @@ async def calc_cards(message: types.Message):
 
         if player_list and table_cards:
             for player in player_list:
-                print(f"{player.name}'s full hand:", player.hand + table_cards)
-                player.score, player.combination = dealer.calc_score(player.hand + table_cards);
-                print(f"{player.name}'s has combination: {player.combination} score: {player.score}")
+                if player.action != Action.FOLD:
+                    print(f"{player.name}'s full hand:", player.hand + table_cards)
+                    player.score, player.combination = dealer.calc_score(player.hand + table_cards);
+                    print(f"{player.name}'s has combination: {player.combination} score: {player.score}")
 
             max_score = max(player_list, key=lambda player: player.score).score
+
+            table_cards = []
+            dealer.shuffle_deck()
+
             win_list = [player for player in player_list if player.score == max_score]
             output = ''
             for player in win_list:
                 output = output + (f"{player.name} wins! combination: {player.combination} score: {player.score}\n")
+
+            await message.edit_text(f"Table: [][][]", reply_markup=main_kb)
             await message.answer(output)
-
-            player_list = []
-            table_cards = []
-            dealer.shuffle_deck()
-
         else:
-            await message.edit_text(f"{message.text}\n\nThe game has not started. Deal the cards", reply_markup=inline_kb)
+            await message.edit_text(f"{message.text}\n\nThe game has not started. Deal the cards", reply_markup=main_kb)
     except Exception as e:
         await message.answer(f"Something goes wrong: {e}")
+
 
 @dp.message(Command('clean'))
 async def delete_bot_messages(message: types.Message):
@@ -129,32 +136,101 @@ async def delete_bot_messages(message: types.Message):
 
     await message.answer("All bot messages have been deleted!")
 
+
 @dp.message(Command('game'))
 async def game(message: types.Message):
     print(f"Game message {message}")
-    await message.answer(f"Poker game", reply_markup=inline_kb)
+    global main_msg
+    player_list = []
+    main_msg = await message.answer(f"Table: [][][]", reply_markup=start_kb)
 
-@dp.callback_query(lambda callback: callback.data == "handshake")
-async def button_handshake(callback: CallbackQuery):
-    print(f'Callback {callback.from_user.first_name}')
-    await deal_hand(callback)
-    await callback.answer('Deal a hand')
 
-@dp.callback_query(lambda callback: callback.data == "calc")
-async def button_calculation(callback: CallbackQuery):
-    print(f"Callback {callback}")
-    await calc_cards(callback.message)
-    await callback.answer('Calculate score')
+@dp.callback_query(lambda callback: callback.data == "join")
+async def button_join(callback: CallbackQuery):
+    if table_cards:
+        await callback.answer('The game is in progress. Try when table is empty')
+    else:
+        await deal_hand(callback)
+        await callback.answer('Joined the game')
 
-@dp.callback_query(lambda callback: callback.data == "table")
-async def button_calculation(callback: CallbackQuery):
-    await deal_table(callback.message)
-    await callback.answer('Deal a table')
+
+@dp.callback_query(lambda callback: callback.data == "leave")
+async def button_leave(callback: CallbackQuery):
+    for player in player_list:
+        if player.name == callback.from_user.first_name:
+            player_list.remove(player)
+            await main_msg.edit_text(gen_main_view_text(), parse_mode='MarkdownV2', reply_markup=start_kb)
+            await callback.answer('You left the game')
+
+@dp.callback_query(lambda callback: callback.data == "check")
+async def button_check(callback: CallbackQuery):
+    name = callback.from_user.first_name
+    id = callback.from_user.id
+    if is_authorized(callback.message.message_id, name):
+        if any(player.id == callback.from_user.id and player.action != Action.FOLD for player in player_list):
+            set_action_by_name(name, Action.CHECK)
+            await process_round()
+            await callback.answer(f'{callback.from_user.first_name} check')
+        else:
+            await callback.answer('You already submit fold')
+    else:
+        await callback.answer('You are not allowed to do this')
+
+
+@dp.callback_query(lambda callback: callback.data == "fold")
+async def button_fold(callback: CallbackQuery):
+    name = callback.from_user.first_name
+    if is_authorized(callback.message.message_id, name):
+        set_action_by_name(name, Action.FOLD)
+        await process_round()
+        await callback.answer(f'{name} fold')
+    else:
+        await callback.answer('You are not allowed to do this')
+
+
+def set_action_by_name(name, action):
+    for player in player_list:
+        if player.name == name and player.action != Action.FOLD:
+            print(f"Set action {player.action.value} for {player.name}")
+            player.set_action(action)
+            print(f"Current action {player.action.value} for {player.name}")
+
+
+def is_round_done():
+    return not any(player.action == Action.NONE for player in player_list)
+
+
+def gen_main_view_text():
+    player_list_str = '\nPlayer list: \n'
+    for player in player_list:
+        player_list_str = player_list_str + f"{player_list.index(player)} {player.name} {player.action.value} {player.bank}\n"
+    if table_cards:
+        return f"Table: {table_cards}\n" + player_list_str
+    else:
+        return player_list_str
+
+
+async def process_round():
+    if is_round_done():
+        for player in player_list:
+            player.set_action(Action.NONE)
+        if len(table_cards) == 5:
+            await calc_cards(main_msg)
+        else:
+            await deal_table(main_msg)
+    else:
+        await main_msg.edit_text(gen_main_view_text(), parse_mode='MarkdownV2', reply_markup=main_kb)
+
+
+def is_authorized(message_id, name):
+    return any(msg_map[message_id] == player.id and player.name == name for player in player_list)
+
 
 async def main():
     await bot(DeleteWebhook(drop_pending_updates=True))
     print("Ready to start")
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     dealer.shuffle_deck()
--- player.py
+++ player.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 class Player:
     def __init__(self, name):
         self.id = ''
@@ -6,7 +8,7 @@ class Player:
         self.score = 0
         self.bank = 0
         self.combination = ''
-        self.action = ''
+        self.action = Action.NONE
 
     def add_card_to_hand(self, card):
         self.hand.append(card)
@@ -26,8 +28,21 @@ class Player:
     def set_bank(self, bank):
         self.bank = bank
 
+    def set_action(self, action):
+        self.action = action
+
+    def set_id(self, id):
+        self.id = id
+
     def __str__(self):
         return (f"Player: {self.name}\n"
                 f"Hand: {self.hand}\n"
                 f"Score: {self.score}\n"
                 f"Bank: ${self.bank}")
+
+class Action(Enum):
+    NONE = 'None'
+    CHECK = 'Check'
+    CALL = 'Call'
+    RAISE = 'Raise'
+    FOLD = 'Fold'
